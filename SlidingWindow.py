a36c9399dbb5172378a1755543943e802c248da1
ShafiaAnsar <shafiaansar142@gmail.com>
2023-04-08 13:45:54 +0500

    SlidingWindow

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -1,10 +1,3 @@
-# nums = [1,2,3]
-# sum(nums)
-# for n in nums:
-#     print(n)
-# nums.insert(1, 100)
-# nums.remove(100)
-# print(100 in nums)
 def find_ave(K , arr):
     result=[]
     windowSum , windowStart = 0.0 ,0
