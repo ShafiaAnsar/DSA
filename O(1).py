70ba426b1b5a0c4740c416fc693b3d0d842efe6c
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-08 10:57:06 +0500

    Big-O Notation

---
+++ O(1).py
@@ -0,0 +1,14 @@
+#Array
+nums = [1,2,3]
+nums.append(4)
+nums.pop()
+print(nums[0])
+nums[1]
+nums[2]
+
+#Set
+hashMap =  {}
+hashMap["key"] = 10
+print("key" in hashMap )
+print(hashMap["key"])
+print(hashMap.pop("key"))
